3d582f07066de488fbe9d4de3f551cc97a0160cd
Will Farrington <wfarr@github.com>
2013-02-13 21:51:10 -0800

    bump cardboard dependency

--- spec/classes/watts_spec.rb
+++ spec/classes/watts_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'watts' do
   it do
     should contain_package('Watts').with({
-      :provider => 'pkgdmg',
+      :provider => 'appdmg',
       :source   => 'http://www.matchingbrackets.com/abatt/customer/Watts-48.dmg',
     })
   end
